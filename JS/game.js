c10ce7abc88a5c3121e93bba46163af342d1f569
Alex Balmes <alex.balmes1912@gmail.com>
2022-11-29 20:07:43 +0100

    add of a replay function and validation animation

--- JS/game.js
+++ JS/game.js
@@ -1,61 +1,108 @@
 let btn = document.getElementsByClassName('button')
-
-btn[0].addEventListener('click', (e) => {
-    btn[0].classList.toggle('button--light')
-    setTimeout(() =>{
-        btn[0].classList.toggle('button--light')
-    },300)
-});
-
-btn[1].addEventListener('click', (e) => {
-    btn[1].classList.toggle('button--light')
-    setTimeout(() =>{
-        btn[1].classList.toggle('button--light')
-    },300)
-});
-
-btn[2].addEventListener('click', (e) => {
-    btn[2].classList.toggle('button--light')
-    setTimeout(() =>{
-        btn[2].classList.toggle('button--light')
-    },300)
-});
-
-btn[3].addEventListener('click', (e) => {
-    btn[3].classList.toggle('button--light')
-    setTimeout(() =>{
-        btn[3].classList.toggle('button--light')
-    },300)
-});
+let pressValue = []
+let colorList = []
 
 const getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
-  }
-  
+    return colorList.push(Math.floor(Math.random() * max));
+    
+}
 
+const randomFlash = (number) => {
   setTimeout(() => {
-    let rdm = getRandomInt(4)
-    btn[rdm].classList.toggle('button--light')
-    setTimeout(() =>{
-        btn[rdm].classList.toggle('button--light')
-    },600)
-    console.log(rdm)
-  }, 1500)
+      btn[colorList[number]].classList.toggle('button--light')
+      setTimeout(() =>{
+        btn[colorList[number]].classList.toggle('button--light')
+      },600)
+    }, 1150 * number + 1)
+}
 
-  setTimeout(() => {
-    let rdm = getRandomInt(4)
-    btn[rdm].classList.toggle('button--light')
-    setTimeout(() =>{
-        btn[rdm].classList.toggle('button--light')
-    },600)
-    console.log(rdm)
-  }, 3000)
+const newGame = () => {
+    getRandomInt(4)
+    getRandomInt(4)
+    getRandomInt(4)
+    randomFlash(0)
+    randomFlash(1)
+    randomFlash(2)
+}
 
-  setTimeout(() => {
-    let rdm = getRandomInt(4)
-    btn[rdm].classList.toggle('button--light')
-    setTimeout(() =>{
-        btn[rdm].classList.toggle('button--light')
-    },600)
-    console.log(rdm)
-  }, 4500)
+const PressingButton = (number) => {
+  btn[number].addEventListener('click', (e) => {
+      btn[number].classList.toggle('button--light')
+      setTimeout(() =>{
+        btn[number].classList.toggle('button--light')
+      },300)
+      return pressValue.push(number)
+  });
+}
+
+PressingButton(0)
+PressingButton(1)
+PressingButton(2)
+PressingButton(3)
+
+newGame()
+console.log(colorList)
+
+const PressCompare = (number) => {
+    btn[number].addEventListener('click', (e) => {
+        console.log(pressValue)
+        if (pressValue.length == 1)
+            if (colorList[0] == pressValue[0]){
+                document.getElementsByClassName('background')[0].classList.toggle('background--true')
+                setTimeout(() =>{
+                    document.getElementsByClassName('background')[0].classList.toggle('background--true')
+                  },300)
+            }
+            else{
+                document.getElementsByClassName('background')[0].classList.toggle('background--false')
+                setTimeout(() =>{
+                    document.getElementsByClassName('background')[0].classList.toggle('background--false')
+                  },300)
+                pressValue = []
+                colorList = []
+                newGame()
+            }
+        if (pressValue.length == 2)
+            if (colorList[1] == pressValue[1]){
+                document.getElementsByClassName('background')[0].classList.toggle('background--true')
+                setTimeout(() =>{
+                    document.getElementsByClassName('background')[0].classList.toggle('background--true')
+                  },300)
+            }
+            else{
+                document.getElementsByClassName('background')[0].classList.toggle('background--false')
+                setTimeout(() =>{
+                    document.getElementsByClassName('background')[0].classList.toggle('background--false')
+                  },300)
+                pressValue = []
+                colorList = []
+                newGame()
+            }
+        if (pressValue.length == 3)
+            if (colorList[2] == pressValue[2]){
+                document.getElementsByClassName('background')[0].classList.toggle('background--true')
+                setTimeout(() =>{
+                    document.getElementsByClassName('background')[0].classList.toggle('background--true')
+                  },300)
+                setTimeout(() =>  {
+                    pressValue = []
+                    colorList = []
+                    newGame()
+                }, 1000) 
+            }
+            else{
+                document.getElementsByClassName('background')[0].classList.toggle('background--false')
+                setTimeout(() =>{
+                    document.getElementsByClassName('background')[0].classList.toggle('background--false')
+                  },300)
+                pressValue = []
+                colorList = []
+                newGame()
+            }
+    })
+}
+
+for (let i = 0; i < 4; i++) {
+    PressCompare(i)
+}
+
