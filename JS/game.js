7c56ffd9089eceecee18e91305f607660357b551
Alex Balmes <alex.balmes1912@gmail.com>
2022-11-30 16:57:48 +0100

    modification of JS but with some problem to solve

--- JS/game.js
+++ JS/game.js
@@ -2,6 +2,7 @@ let btn = document.getElementsByClassName('button')
 let pressValue = []
 let colorList = []
 let score = 0
+let gameStep = 3
 
 const getRandomInt = (max) => {
     return colorList.push(Math.floor(Math.random() * max));
@@ -18,83 +19,69 @@ const randomFlash = (number) => {
 }
 
 const newGame = () => {
+  for (let i = 0; i != gameStep ; i++){
     getRandomInt(4)
-    getRandomInt(4)
-    getRandomInt(4)
-    randomFlash(0)
-    randomFlash(1)
-    randomFlash(2)
+    randomFlash(i)
+  }
+    console.log(colorList)
 }
 
+const lose = () => {
+  document.getElementsByClassName('background')[0].classList.toggle('background--false')
+  setTimeout(() =>{
+      document.getElementsByClassName('background')[0].classList.toggle('background--false')
+    },300)
+   pressValue = []
+   colorList = []
+    score = 0
+    gameStep = 3
+    console.log(gameStep)
+    newGame()
+}
 
 newGame()
-console.log(colorList)
 
-const Game = (number) => {
-    console.log(colorList)
+const Game = (number, gamestepnum) => {
     btn[number].addEventListener('click', (e) => {
         btn[number].classList.toggle('button--light')
       setTimeout(() =>{
         btn[number].classList.toggle('button--light')
       },300)
       pressValue.push(number)
-      console.log(pressValue)
-        if (pressValue.length == 1)
-            if (colorList[0] == pressValue[0]){
-                  score = score + 100
-            }
-            else{
-                document.getElementsByClassName('background')[0].classList.toggle('background--false')
-                setTimeout(() =>{
-                    document.getElementsByClassName('background')[0].classList.toggle('background--false')
-                  },300)
-                pressValue = []
-                colorList = []
-                score = 0
-                newGame()
-            }
-        if (pressValue.length == 2)
-            if (colorList[1] == pressValue[1]){
-                  score = score + 200
-            }
-            else{
-                document.getElementsByClassName('background')[0].classList.toggle('background--false')
-                setTimeout(() =>{
-                    document.getElementsByClassName('background')[0].classList.toggle('background--false')
-                  },300)
-                pressValue = []
-                colorList = []
-                score = 0
-                newGame()
-            }
-        if (pressValue.length == 3)
-            if (colorList[2] == pressValue[2]){
-                document.getElementsByClassName('background')[0].classList.toggle('background--true')
-                setTimeout(() =>{
-                    document.getElementsByClassName('background')[0].classList.toggle('background--true')
-                  },300)
-                  score = score * 2 
-                setTimeout(() =>  {
-                    pressValue = []
-                    colorList = []
-                    newGame()
-                }, 1000) 
-            }
-            else{
-                document.getElementsByClassName('background')[0].classList.toggle('background--false')
-                setTimeout(() =>{
-                    document.getElementsByClassName('background')[0].classList.toggle('background--false')
-                  },300)
-                pressValue = []
-                colorList = []
-                score = 0
-                newGame()
-            }
+      if (pressValue.length == gamestepnum){
+        if (colorList[gamestepnum - 1] == pressValue[gamestepnum - 1]){
+          document.getElementsByClassName('background')[0].classList.toggle('background--true')
+          setTimeout(() =>{
+              document.getElementsByClassName('background')[0].classList.toggle('background--true')
+            },300)
+            score = score * 2 
+          setTimeout(() =>  {
+              pressValue = []
+              colorList = []
+              newGame()
+          }, 1000) 
+          gameStep = gameStep + 1  
+        }
+        else{
+          lose()
+        }
+      }
+
+      for(let i = 0; i < gamestepnum; i++) {
+      if (pressValue.length == (i +1))
+          if (colorList[i] == pressValue[i]){
+                score = score + 100
+           }
+          else{
+             lose()
+          }
+      }
         document.getElementsByClassName('score__number')[0].innerHTML = score
+        console.log(gamestepnum + 'debut detape')
+        console.log(gameStep + 'etape globale')
     })
 }
 
 for (let i = 0; i < 4; i++) {
-    Game(i)
+    Game(i, gameStep)
 }
-
